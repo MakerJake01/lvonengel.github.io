a9da78adbc840e69c99a98f034de5729acc93433
Liu von Engelbrechten <vonengelbrechten.l@northeastern.edu>
2024-11-05 00:57:06 -0500

    created dark mode button

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Add JavaScript code for your web site here
+
+let themeButton = document.getElementById("theme-button");
+
+// TODO: Complete the toggleDarkMode function
+const toggleDarkMode = () => {
+    // Write your code to manipulate the DOM here
+    document.body.classList.toggle("dark-mode");
+
+}
+
+themeButton.addEventListener("click", toggleDarkMode);
